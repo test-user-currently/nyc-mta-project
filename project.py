c07b12099569871b3825ebf18d6a20080c6cf2fd
Laura Goon <lg43@rice.edu>
2018-12-10 13:33:37 -0600

    test commit

--- project.py
+++ project.py
@@ -5,4 +5,4 @@ api_key = 'API_Key'
 
 mta_info = requests.get('http://datamine.mta.info/mta_esi.php?key=' + api_key + '&feed_id=16')
 
-print feed.ParseFromString(mta_info)
+print feed.ParseFromString(mta_info.content)
