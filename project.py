04dc8f904dd4323a4fc4a8c2612cc0ea41ecb49d
Laura Goon <goon.laura@gmail.com>
2018-12-10 21:43:52 -0600

    added station locations csv

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
 from google.transit import gtfs_realtime_pb2
 from protobuf_to_dict import protobuf_to_dict
+import csv
 import requests
+import urllib2
 
 # --------- RETRIEVING DATA --------- #
 
@@ -19,13 +21,14 @@ response_NQRW = requests.get('http://datamine.mta.info/mta_esi.php?key=' + api_k
 feed_NQRW.ParseFromString(response_NQRW.content)
 dict_NQRW = protobuf_to_dict(feed_NQRW)
 
-# routes data
-routes_file = open('mta-data/routes.txt', 'r')
-routes = routes_file.read()
-routes_file.close()
+# # routes data
+# # route_id,agency_id,route_short_name,route_long_name,route_desc,route_type,route_url,route_color,route_text_color
+# routes_file = open('mta-data/routes.txt', 'r')
+# routes = routes_file.read()
+# routes_file.close()
+# print routes
 
-
-# station stops data
+# station stops data - DONE
 # stop_id,stop_code,stop_name,stop_desc,stop_lat,stop_lon,zone_id,stop_url,location_type,parent_station
 stops_file = open('mta-data/stops.txt', 'r')
 stops = stops_file.read()
@@ -35,6 +38,15 @@ stops_file.close()
 trips_file = open('mta-data/trips.txt', 'r')
 trips = trips_file.read()
 trips_file.close()
+
+# station locations data
+# Station ID,Complex ID,GTFS Stop ID,Division,Line,Stop Name,Borough,Daytime Routes,Structure,GTFS Latitude,GTFS Longitude
+csv_url = 'http://web.mta.info/developers/data/nyct/subway/Stations.csv'
+response = urllib2.urlopen(csv_url)
+cr = csv.reader(response)
+locations_file = []
+for row in cr:
+    locations_file.append(row)
 
 # --------- RETRIEVING DATA (END) --------- #
 
@@ -46,13 +58,9 @@ trips_file.close()
 # - station name
 # - time enter/leave station
 
-# def organize_route_data(data):
-#     new_data = data.split("\n")[1:]
-#     for ()
-#     return new_data[0]
 
-# does not return anything; populates stops_to_name dictionary
-def organize_stops_data(data):
+# does not return anything; populates stops_name_to_id dictionary
+def generate_stopsNameToID(data): # --------- FIX BC SOME NAMES ARE DIFF BUT SAME STOP --------
     new_data = data.split("\n")[1:-1]
     data_len = len(new_data)
 
@@ -63,7 +71,7 @@ def organize_stops_data(data):
 
     ret_dict = {}
     for idx in range(data_len):
-        ret_dict[stops_id[idx]] = stops_name[idx]
+        ret_dict[stops_name[idx]] = stops_id[idx]
     return ret_dict
 
 # {trainLetter: [{stationName : (time enter, time leave)}, {stationName : (time enter, time leave)}], trainLetter: [{stationName : (time enter, time leave)}]}
@@ -79,16 +87,15 @@ def organize_stops_data(data):
 
 # --------- DATA TO USE --------- #
 
-stops_id_to_name = organize_stops_data(stops) # mapping from stop_id (string) -> stop_name (string)
+stops_name_to_id = generate_stopsNameToID(stops) # mapping from stop_name (string) -> stop_id (string)
+lines_to_stops = {} # mapping from line names (string) -> stop names (list of strings)
 
 # --------- DATA TO USE (END) --------- #
 
 
 
 # --------- TESTS --------#
-# print stops_id_to_name
-
-# print organize_stops_data(stops)
+# print stops_name_to_id["Astoria - Ditmars Blvd"]
 
 # print dict_BDFM.get("entity")[0]
 # print dict_BDFM.get("entity")[1]
