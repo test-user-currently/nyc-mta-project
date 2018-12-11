482b87b877c4bd91332da8a4bb2961ae36687c7b
Laura Goon <goon.laura@gmail.com>
2018-12-10 19:44:03 -0600

    added mapping from stop id -> stop name

--- project.py
+++ project.py
@@ -23,9 +23,10 @@ dict_NQRW = protobuf_to_dict(feed_NQRW)
 routes_file = open('mta-data/routes.txt', 'r')
 routes = routes_file.read()
 routes_file.close()
-print routes
+
 
 # station stops data
+# stop_id,stop_code,stop_name,stop_desc,stop_lat,stop_lon,zone_id,stop_url,location_type,parent_station
 stops_file = open('mta-data/stops.txt', 'r')
 stops = stops_file.read()
 stops_file.close()
@@ -35,26 +36,35 @@ trips_file = open('mta-data/trips.txt', 'r')
 trips = trips_file.read()
 trips_file.close()
 
-
-
-# print dict_BDFM.get("entity")[0]
-# print dict_BDFM.get("entity")[1]
-# print dict_BDFM.get("entity")[2]
-# print dict_BDFM.get("entity")[3]
-# print dict_BDFM.get("entity")[4]
-
 # --------- RETRIEVING DATA (END) --------- #
 
 
-# --------- DOING THINGS --------- #
+
+# --------- ORGANIZING DATA --------- #
 # NEED:
 # - subway letter
 # - station name
 # - time enter/leave station
 
-def organize_route_data(data):
-    new_data = data.split()[1:]
-    return new_data
+# def organize_route_data(data):
+#     new_data = data.split("\n")[1:]
+#     for ()
+#     return new_data[0]
+
+# does not return anything; populates stops_to_name dictionary
+def organize_stops_data(data):
+    new_data = data.split("\n")[1:-1]
+    data_len = len(new_data)
+
+    for idx in range(data_len):
+        new_data[idx] = new_data[idx].split(",")
+    stops_id = map(lambda x: x[0], new_data)
+    stops_name = map(lambda x: x[2], new_data)
+
+    ret_dict = {}
+    for idx in range(data_len):
+        ret_dict[stops_id[idx]] = stops_name[idx]
+    return ret_dict
 
 # {trainLetter: [{stationName : (time enter, time leave)}, {stationName : (time enter, time leave)}], trainLetter: [{stationName : (time enter, time leave)}]}
 
@@ -63,11 +73,28 @@ def organize_route_data(data):
 #     if entity.hasField('trip_update'):
 #         print entity.trip_update.trip.
 
-# --------- DOING THINGS (END) --------- #
+# --------- ORGANIZING DATA (END) --------- #
+
+
+
+# --------- DATA TO USE --------- #
+
+stops_id_to_name = organize_stops_data(stops) # mapping from stop_id (string) -> stop_name (string)
+
+# --------- DATA TO USE (END) --------- #
+
 
 
 # --------- TESTS --------#
-# print str(feed_BDFM)
+# print stops_id_to_name
+
+# print organize_stops_data(stops)
+
+# print dict_BDFM.get("entity")[0]
+# print dict_BDFM.get("entity")[1]
+# print dict_BDFM.get("entity")[2]
+# print dict_BDFM.get("entity")[3]
+# print dict_BDFM.get("entity")[4]
 
 
 
