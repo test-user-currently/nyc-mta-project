a3d65c2854e3c81d7cf0214f5f8178b3bb844b4b
Laura Goon <goon.laura@gmail.com>
2018-12-13 17:16:30 -0600

    created a mapping from lines -> trains

--- project.py
+++ project.py
@@ -21,11 +21,12 @@ response_NQRW = requests.get('http://datamine.mta.info/mta_esi.php?key=' + api_k
 feed_NQRW.ParseFromString(response_NQRW.content)
 dict_NQRW = protobuf_to_dict(feed_NQRW)
 
-# # routes data
-# # route_id,agency_id,route_short_name,route_long_name,route_desc,route_type,route_url,route_color,route_text_color
-# routes_file = open('mta-data/routes.txt', 'r')
-# routes = routes_file.read()
-# routes_file.close()
+# routes data
+# route_id,agency_id,route_short_name,route_long_name,route_desc,route_type,route_url,route_color,route_text_color
+# H = S line
+routes_file = open('mta-data/routes.txt', 'r')
+routes = routes_file.read()
+routes_file.close()
 # print routes
 
 # station stops data - DONE
@@ -81,6 +82,26 @@ def generate_stopsNameToID(data): # --------- FIX BC SOME NAMES ARE DIFF BUT SAM
 #     if entity.hasField('trip_update'):
 #         print entity.trip_update.trip.
 
+def generate_linesToTrains(data):
+    new_data = data.split("\n")[1:-1]
+    ret_dict = {}
+    for line in new_data:
+        line = line.split(",")
+
+        line_fixed = line[3].replace(" Local", "").replace(" Express", "") # combine the local and express lines
+        # some line name formats not centralized
+        if "6 Av" in line_fixed or "6th AVE" in line_fixed:
+            line_fixed = "6 Avenue"
+        if "7 Avenue" in line_fixed:
+            line_fixed = "7 Avenue"
+
+        if line_fixed not in ret_dict.keys():
+            ret_dict[line_fixed] = [line[0]]
+        else:
+            ret_dict[line_fixed].append(line[0])
+
+    return ret_dict
+
 # --------- ORGANIZING DATA (END) --------- #
 
 
@@ -88,7 +109,8 @@ def generate_stopsNameToID(data): # --------- FIX BC SOME NAMES ARE DIFF BUT SAM
 # --------- DATA TO USE --------- #
 
 stops_name_to_id = generate_stopsNameToID(stops) # mapping from stop_name (string) -> stop_id (string)
-lines_to_stops = {} # mapping from line names (string) -> stop names (list of strings)
+lines_to_trains = generate_linesToTrains(routes) # mapping from line names (string) -> trains (list of strings)
+trains_to_stops = {} # mapping from trains (string) -> stop names (list of strings)
 
 # --------- DATA TO USE (END) --------- #
 
@@ -97,6 +119,11 @@ lines_to_stops = {} # mapping from line names (string) -> stop names (list of st
 # --------- TESTS --------#
 # print stops_name_to_id["Astoria - Ditmars Blvd"]
 
+# print lines_to_trains
+
+# print locations_file
+
+# print dict_BDFM
 # print dict_BDFM.get("entity")[0]
 # print dict_BDFM.get("entity")[1]
 # print dict_BDFM.get("entity")[2]
